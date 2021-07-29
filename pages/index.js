56dd9fe7b28c9af71b84a2f5b5c562c1db71dc5c
Aviraj Sharma Ramhith <aramhith@proximitybbdo.fr>
2021-07-29 13:02:21 +0400

    static props

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
+import path from "path";
+import Link from "next/link";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}> {product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  console.log("re generating");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      products: [{ id: "p1", title: "prdwq1" }],
+      products: data.products,
     },
+    revalidate: 10,
   };
 }
 
